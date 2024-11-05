fd6a14f75c7cef48df42659ea85ac6ae8b28b4c8
Lucas <luagostino@uade.edu.ar>
2024-11-05 16:35:14 -0300

    modificacion para ver los datos en consola

--- Api-Front/src/api/AxiosConfig.jsx
+++ Api-Front/src/api/AxiosConfig.jsx
@@ -1,15 +1,15 @@
 // axiosConfig.js
 import axios from 'axios';
-import store from '../api/Store';
+import {store} from '../api/Store';
 
 const api = axios.create({
   baseURL: 'http://localhost:8080', // Cambia esto por la URL base de tu API
 });
-
 // Interceptor para añadir el token a las solicitudes que lo necesiten
 api.interceptors.request.use(
   (config) => {
     const token = store.getState().user.token; // Obtiene el token del estado de Redux
+    console.log('Store:', store.getState());
     if (token) {
       config.headers.Authorization = `Bearer ${token}`; // Añade el token a las cabeceras
     }
